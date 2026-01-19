f9bade84cef5b24960b1407db22fcc920945245a
Aniket <aniketsingh2151@gmail.com>
2026-01-19 18:54:15 +0530

    done with adding system prompts. need to check if the system parameter in openRouter.chat.send() function is indeed sending the prompt or not, since it seems to be giving an error. the frontend on bolt.new also sends an additional prompt coating the user message, need to include that too

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,5 +1,30 @@
-require("dotenv").config();
+require("dotenv-mono").load();
 
-console.log(process.env.API_KEY)
+// console.log(process.env.API_KEY)
+const OPENROUTER_API_KEY = process.env.API_KEY;
 
-console.log("Hello via Bun!");
+import { OpenRouter } from '@openrouter/sdk';
+import { getSystemPrompt } from './prompts.ts';
+
+const openRouter = new OpenRouter({
+  apiKey: OPENROUTER_API_KEY,
+//   defaultHeaders: {
+//     'HTTP-Referer': '<YOUR_SITE_URL>', // Optional. Site URL for rankings on openrouter.ai.
+//     'X-Title': '<YOUR_SITE_NAME>', // Optional. Site title for rankings on openrouter.ai.
+//   },
+});
+
+const completion = await openRouter.chat.send({
+  model: 'google/gemini-2.5-flash',
+  system: getSystemPrompt(),
+  messages: [
+    {
+      role: 'user',
+      content: 'What is 2+2 = ?',
+    }
+  ],
+  maxTokens: 64,
+  stream: false,
+});
+
+console.log(completion.choices[0]!.message.content);
